0da43f9af9262461ae93805e1713d005e907770d
timing-arg <timingdesarrollos@gmail.com>
2023-12-27 14:34:08 -0300

    se agrega index.html

--- django/tisoco/core/urls.py
+++ django/tisoco/core/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index, name='index')
+    path('',views.index, name='index'),
+    path('productos/detalle/<str:nombre_producto>', views.producto_detalle, name='productos_detalle')
+
 ]
--- django/tisoco/core/views.py
+++ django/tisoco/core/views.py
@@ -2,6 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("hola mundo parte 2")
+    return render(request, "index.html")
 
+def producto_detalle(request, nombre_producto):
+    return HttpResponse(
+        f"""
+            <h1>Bienvenid@ {nombre_producto} </h1>
+            <p>Detalle del producto</p>
+        """
+    )
 # Create your views here.
